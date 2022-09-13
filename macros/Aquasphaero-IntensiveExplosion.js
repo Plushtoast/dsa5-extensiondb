027b98937d770a90250ce95c35d2ffe96817ca5c
Feiertag, Nico <nfeiertag@gmx.net>
2022-09-13 21:04:46 +0200

    Adding Aquasphaero Intensive Explosion Skript

---
+++ macros/Aquasphaero-IntensiveExplosion.js
@@ -0,0 +1,21 @@
+// transform spell source data object
+
+let effect = source.effects.find(x => x.label == "Aquasphaero")
+
+if(!effect) return
+
+effect = duplicate(effect)
+source.effects = source.effects.filter(x => x.label != "Aquasphaero")
+
+const lang = game.i18n.lang == "de" ? "de" : "en"
+const dict = {
+    de: {
+        msg: "wurde von den Wassermengen betäubt"
+    },
+    en: {
+        msg: "was stunned by the gush of water"
+    }
+}[lang]
+
+effect.flags.dsa5.args3 = `msg += \` \${actor.name} ${dict.msg}.\`;\nawait actor.addCondition('stunned', 1, false)`
+source.effects.push(effect)
