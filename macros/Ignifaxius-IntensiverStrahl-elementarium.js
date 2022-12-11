de098f76b36adf3fe7fd4f52f883bea3d8d6c3af
Feiertag, Nico <nfeiertag@gmx.net>
2022-12-11 21:50:28 +0100

    adapting all the spells

---
+++ macros/Ignifaxius-IntensiverStrahl-elementarium.js
@@ -0,0 +1,31 @@
+// transform spell source data object
+
+let effect = source.effects.find(x => x.label == "Ignifaxius")
+
+if(!effect) return
+
+effect = duplicate(effect)
+source.effects = source.effects.filter(x => x.label != "Ignifaxius")
+
+const lang = game.i18n.lang == "de" ? "de" : "en"
+const dict = {
+    de: {
+        msg: "wurde in Brand gesteckt"
+    },
+    en: {
+        msg: "was set on fire"
+    }
+}[lang]
+
+const resist = {
+    de: {
+        msg: "wurde nicht in brand gesteckt"
+    },
+    en: {
+        msg: "did not catch fire"
+    }
+}[lang]
+
+// size and check (for big) in the effect to do this for eah target individually
+effect.flags.dsa5.args3 = `const effect_name  = 'burning';let size = actor.system.status.size.value;if(size=='giant'){msg += \` \${actor.name} ${resist.msg}.\`;}else if(size=='big'){let roll = new Roll("1d6"); let d6 = roll.evaluate({async:false}).result;let render = await roll.render();if (d6<=4) {msg += \` \${actor.name} ${dict.msg}.\`+render ;\nawait actor.addCondition(effect_name);} else{msg += \` \${actor.name} ${resist.msg}.\`+render;} } else {msg += \` \${actor.name} ${dict.msg}.\`;\nawait actor.addCondition(effect_name)}`
+source.effects.push(effect)
