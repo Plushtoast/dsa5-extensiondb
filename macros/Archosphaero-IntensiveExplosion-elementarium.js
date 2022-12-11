d87b3c806a265382259b9d5a4128b1c91fd213b8
Feiertag, Nico <nfeiertag@gmx.net>
2022-12-11 19:48:42 +0100

    adding AquaSphaero/Faxius

---
+++ macros/Archosphaero-IntensiveExplosion-elementarium.js
@@ -0,0 +1,31 @@
+// transform spell source data object
+
+let effect = source.effects.find(x => x.label == "Archosphaero")
+
+if(!effect) return
+
+effect = duplicate(effect)
+source.effects = source.effects.filter(x => x.label != "Archosphaero")
+
+const lang = game.i18n.lang == "de" ? "de" : "en"
+const dict = {
+    de: {
+        msg: "wurde schwer belastet"
+    },
+    en: {
+        msg: "has been encumbered"
+    }
+}[lang]
+
+const resist = {
+    de: {
+        msg: "hat der Belastung widerstanden"
+    },
+    en: {
+        msg: "resisted the encumberment"
+    }
+}[lang]
+
+// size and check (for big) in the effect to do this for eah target individually
+effect.flags.dsa5.args3 = `const effect_name  = 'encumbered';let size = actor.system.status.size.value;if(size=='giant'){msg += \` \${actor.name} ${resist.msg}.\`;}else if(size=='big'){let roll = new Roll("1d6"); let d6 = roll.evaluate({async:false}).result;let render = await roll.render();if (d6<=4) {msg += \` \${actor.name} ${dict.msg}.\`+render ;\nawait actor.addCondition(effect_name, 1, false);} else{msg += \` \${actor.name} ${resist.msg}.\`+render;} } else {msg += \` \${actor.name} ${dict.msg}.\`;\nawait actor.addCondition(effect_name , 1, false)}`
+source.effects.push(effect)
