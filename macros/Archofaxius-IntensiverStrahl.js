87bd65519d2e58bfbf327e90af016d248b5d807d
Feiertag, Nico <nfeiertag@gmx.net>
2023-01-22 19:26:12 +0100

    Magie 1 bis "Auge des Limbus"

--- macros/Archofaxius-IntensiverStrahl.js
+++ macros/Archofaxius-IntensiverStrahl.js
@@ -17,5 +17,15 @@ const dict = {
     }
 }[lang]
 
-effect.flags.dsa5.args3 = `msg += \` \${actor.name} ${dict.msg}.\`;\nawait actor.addCondition(\"encumbered\", 1, false)`
+const resist = {
+    de: {
+        msg: "hat der Belastung widerstanden"
+    },
+    en: {
+        msg: "resisted the encumberment"
+    }
+}[lang]
+
+// size and check (for big) in the effect to do this for eah target individually
+effect.flags.dsa5.args3 = `const effect_name  = 'encumbered';let size = actor.system.status.size.value;if(size=='giant'){msg += \` \${actor.name} ${resist.msg}.\`;}else if(size=='big'){let roll = new Roll("1d6"); let d6 = roll.evaluate({async:false}).result;let render = await roll.render();if (d6<=4) {msg += \` \${actor.name} ${dict.msg}.\`+render ;\nawait actor.addCondition(effect_name, 1, false);} else{msg += \` \${actor.name} ${resist.msg}.\`+render;} } else {msg += \` \${actor.name} ${dict.msg}.\`;\nawait actor.addCondition(effect_name , 1, false)}`
 source.effects.push(effect)
