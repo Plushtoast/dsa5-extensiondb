40635ec6ee9a38b24286d59286029fec1f60188b
Jan Weigmann <janweigmann@web.de>
2024-05-22 23:51:45 +0200

    angriffslust macro

---
+++ macros/angriffslustat.js
@@ -0,0 +1,41 @@
+// transform spell source data object
+const lang = game.i18n.lang == "de" ? "de" : "en"
+const dict = {
+    de: {
+        name: "Angriffslust"
+    },
+    en: {
+        name: "Aggression"
+    }
+}[lang]
+let origEffect = source.effects.find(x => x.name == dict.name)
+
+if(!origEffect) return
+
+origEffect = duplicate(origEffect)
+source.effects = source.effects.filter(x => x.name != dict.name)
+
+origEffect.changes = [
+    {
+    "key": "system.meleeStats.damage",
+    "mode": 2,
+    "value": "+2"
+    },
+    {
+    "key": "system.meleeStats.attack",
+    "mode": 2,
+    "value": 3
+    },
+    {
+    "key": "system.rangeStats.damage",
+    "mode": 2,
+    "value": "+2"
+    },
+    {
+    "key": "system.rangeStats.attack",
+    "mode": 2,
+    "value": 3
+    }
+]
+
+source.effects.push(origEffect)
