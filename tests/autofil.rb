bcd37e9d1f8cd64d14d021f105ca005f1c4f4b39
Jan Weigmann <janweigmann@web.de>
2024-06-08 16:39:39 +0200

    jo

--- tests/autofil.rb
+++ tests/autofil.rb
@@ -3,6 +3,18 @@ require 'json'
 dbPath = "./../dbs/"
 
 
+prettyprint = true
+if prettyprint
+    Dir.entries(dbPath).each do |db|
+        next if db == "." or db == ".."
+        json = File.open("#{dbPath}#{db}") {|f| JSON.parse(f.read)}
+
+        File.open("#{dbPath}#{db}", "w") {|f| f.write(JSON.pretty_generate(json))}
+    end
+    return
+end
+
+
 dataAll = {}
 Dir.entries(dbPath).each do |db|
     next if db == "." or db == ".."
@@ -35,6 +47,8 @@ Dir.entries(dbPath).each do |db|
     next if db == "." or db == ".."
     json = File.open("#{dbPath}#{db}") {|f| JSON.parse(f.read)}
 
+    category = db.split(".")[0].split("_")[-1]
+
     write = false
     json.each do |spell, extensions|
         
@@ -53,4 +67,4 @@ Dir.entries(dbPath).each do |db|
     end
 end
 
-puts JSON.pretty_generate(dataAll)
+puts "Done"
