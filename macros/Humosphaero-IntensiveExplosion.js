3288f939e10bc083ce18e74f5f621a3992086bc7
Nico Feiertag <NicoFeiertag@gmx.net>
2024-06-10 12:10:49 +0200

    * Angleichen Elementarium * Überall "Wand Aus Element" formen übertragen * Macros angleichen * Elementare Sprüche angleichen

--- macros/Humosphaero-IntensiveExplosion.js
+++ macros/Humosphaero-IntensiveExplosion.js
@@ -17,5 +17,16 @@ const dict = {
     }
 }[lang]
 
-origEffect.flags.dsa5.args3 = `msg += \` \${actor.name} ${dict.msg}.\`;\nawait actor.addCondition('fixated')`
+const resist = {
+    de: {
+        msg: "ist den Ranken entkommen"
+    },
+    en: {
+        msg: "broke free of the vines"
+    }
+}[lang]
+
+// size and check (for big) in the effect to do this for eah target individually
+origEffect.flags.dsa5.args3 = `const effect_name  = 'fixated';let size = actor.system.status.size.value;if(size=='giant'){msg += \` \${actor.name} ${resist.msg}.\`;}else if(size=='big'){let roll = new Roll("1d6"); let d6 = (await roll.evaluate()).total;let render = await roll.render();if (d6<=4) {msg += \` \${actor.name} ${dict.msg}.\`+render ;\nawait actor.addCondition(effect_name);} else{msg += \` \${actor.name} ${resist.msg}.\`+render;} } else {msg += \` \${actor.name} ${dict.msg}.\`;\nawait actor.addCondition(effect_name)}`
 source.effects.push(origEffect)
+
