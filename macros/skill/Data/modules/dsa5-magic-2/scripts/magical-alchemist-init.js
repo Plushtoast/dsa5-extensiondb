4b22bf1aec6ad4ea63c49da1409a96d6b72b6f83
Wero999 <felix.kittler@web.de>
2026-02-15 15:13:35 +0100

    Create magical-alchemist-init.js

---
+++ macros/skill/Data/modules/dsa5-magic-2/scripts/magical-alchemist-init.js
@@ -0,0 +1,45 @@
+import MagicalAlchemistDSA5 from './magical-alchemist-dsa5.js';
+import DSA5_Utility from '/systems/dsa5/modules/system/helpers/utility-dsa5.js';
+
+const getMessageFromLi = (li) => game.messages.get(li.dataset.messageId);
+
+const getActorFromMessage = (message) => {
+    return message.speaker?.actor ? game.actors.get(message.speaker.actor) : null;
+};
+
+Hooks.once('init', async function() {
+    await loadTemplates([
+        "modules/dsa5-magic-2/templates/magical-alchemist-dialog.hbs"
+    ]);
+});
+
+Hooks.on('getChatMessageContextOptions', (app, options, c) => {
+    options.push({
+        name: "MAGICAL_ALCHEMIST.alchemistName",
+        icon: '<i class="fas fa-magic"></i>',
+        condition: (li) => {
+            const el = li[0] || li; 
+            const message = getMessageFromLi(el);
+            const data = message?.flags?.data;
+            
+            if (!data || data.postData?.rollType !== 'talent' || message.flags.dsa5?.magicalAlchemistUsed) return false;
+            
+            const level = data.postData.successLevel;
+            if (level === -1 || level === -3) return false;
+
+            const actor = DSA5_Utility.getSpeaker(message.speaker) || getActorFromMessage(message);
+            if (!actor) return false;
+            
+            const traditionName = game.i18n.localize("MAGICAL_ALCHEMIST.tradition");
+
+            return actor.items.some(i => 
+                i.type === "specialability" && 
+                i.name.includes(traditionName)
+            );
+        },
+        callback: (li) => {
+            const el = li[0] || li;
+            MagicalAlchemistDSA5.handleMagicalAlchemist(getMessageFromLi(el));
+        }
+    });
+});
