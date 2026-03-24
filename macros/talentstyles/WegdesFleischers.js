de7bf76f054902f6c765ea74a418915f74af5256
Wero999 <felix.kittler@web.de>
2026-03-24 10:07:59 +0100

    Create WegdesFleischers.js

---
+++ macros/talentstyles/WegdesFleischers.js
@@ -0,0 +1,6 @@
+"name": "Weg des Felischers",
+            "complete": false,
+            "changes": [
+                {"key": "@meleeweapon.(Fleischerbeil|Schlachtermesser).system.pamod.value", "mode": 2, "value": +1},
+                {"key": "@meleeweapon.(Fleischerbeil|Schlachtermesser).system.damage.value", "mode": 2, "value": +1},
+            ]
