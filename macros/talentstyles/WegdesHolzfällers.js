0e11499ea10c14e9ea813e5d58caea6ca1a4a415
Wero999 <felix.kittler@web.de>
2026-03-24 10:17:05 +0100

    Create WegdesHolzfällers.js

---
+++ macros/talentstyles/WegdesHolzfällers.js
@@ -0,0 +1,6 @@
+"name": "Weg des Holzfällers",
+            "complete": false,
+            "changes": [
+                {"key": "@meleeweapon.(Holzfälleraxt).system.pamod.value", "mode": 2, "value": +1},
+                {"key": "@meleeweapon.(Holzfälleraxt).system.damage.value", "mode": 2, "value": +1},
+            ]
