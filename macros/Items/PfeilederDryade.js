f9619f9157af3f96534835d4232c1a32b5a2d6fd
Wero999 <felix.kittler@web.de>
2026-03-26 11:40:20 +0100

    Create PfeilederDryade.js

---
+++ macros/Items/PfeilederDryade.js
@@ -0,0 +1,8 @@
+"name": "Pfeile der Dryade",
+            "complete": true,
+            "changes": [
+                {"key": "system.rangeStats.attack", "mode": 2, "value": -2},
+                {"key": "system.meleeStats.attack", "mode": 2, "value": -2},
+                {"key": "system.status.dodge.gearmodifier", "mode": 2, "value": -2},
+                {"key": "system.meleeStats.parry", "mode": 2, "value": -2}
+            ]
