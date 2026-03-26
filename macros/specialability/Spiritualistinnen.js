72446028b55839bd8caecc5926575c8eb63008d1
Wero999 <felix.kittler@web.de>
2026-03-26 22:23:01 +0100

    Create Spiritualistinnen.js

---
+++ macros/specialability/Spiritualistinnen.js
@@ -0,0 +1,9 @@
+"name": "Spiritualistinnen (Seherin von Heute und Morgen) ",
+            "complete": true,
+            "changes": [
+                {"key": "system.skillModifiers.step", "mode": , "value": Geisterbeschwörung 1},
+                {"key": "system.skillModifiers.step", "mode": , "value": Geisterruf 1},
+                {"key": "system.skillModifiers.step", "mode": , "value": Nekropathia 1},
+                {"key": "system.skillModifiers.step", "mode": , "value": Geisteressenz 1},
+                {"key": "system.skillModifiers.step", "mode": , "value": Heptagramma 1},
+            ]
