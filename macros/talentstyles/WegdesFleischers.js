4d493a198ef4e943bd8df293e01846983d2c3d12
Wero999 <felix.kittler@web.de>
2026-03-26 11:37:59 +0100

    Update WegdesFleischers.js

--- macros/talentstyles/WegdesFleischers.js
+++ macros/talentstyles/WegdesFleischers.js
@@ -2,5 +2,5 @@
             "complete": false,
             "changes": [
                 {"key": "@meleeweapon.(Fleischerbeil|Schlachtermesser).system.pamod.value", "mode": 2, "value": +1},
-                {"key": "@meleeweapon.(Fleischerbeil|Schlachtermesser).system.damage.value", "mode": 2, "value": +1},
+                {"key": "@meleeweapon.(Fleischerbeil|Schlachtermesser).system.damage.value", "mode": 2, "value": +1}
             ]
