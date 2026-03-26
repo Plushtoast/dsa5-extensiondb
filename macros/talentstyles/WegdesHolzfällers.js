18311a4384a1267a9daad91a47f2505da4a17111
Wero999 <felix.kittler@web.de>
2026-03-26 11:40:44 +0100

    Update WegdesHolzfällers.js

--- macros/talentstyles/WegdesHolzfällers.js
+++ macros/talentstyles/WegdesHolzfällers.js
@@ -2,5 +2,5 @@
             "complete": false,
             "changes": [
                 {"key": "@meleeweapon.(Holzfälleraxt).system.pamod.value", "mode": 2, "value": +1},
-                {"key": "@meleeweapon.(Holzfälleraxt).system.damage.value", "mode": 2, "value": +1},
+                {"key": "@meleeweapon.(Holzfälleraxt).system.damage.value", "mode": 2, "value": +1}
             ]
