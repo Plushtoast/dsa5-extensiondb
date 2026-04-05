021754587c99ffe9a7fb5a16e52bf59dfff2c17a
Wero999 <felix.kittler@web.de>
2026-04-05 17:39:07 +0200

    Update Spiritualistinnen.js

--- macros/specialability/Spiritualistinnen.js
+++ macros/specialability/Spiritualistinnen.js
@@ -3,7 +3,7 @@
             "changes": [
                 {"key": "system.skillModifiers.step", "mode": , "value": Geisterbeschwörung 1},
                 {"key": "system.skillModifiers.step", "mode": , "value": Geisterruf 1},
-                {"key": "system.skillModifiers.step", "mode": , "value": Nekropathia 1},
+             //   {"key": "system.skillModifiers.step", "mode": , "value": Nekropathia 1},
                 {"key": "system.skillModifiers.step", "mode": , "value": Geisteressenz 1},
                 {"key": "system.skillModifiers.step", "mode": , "value": Heptagramma 1},
             ]
